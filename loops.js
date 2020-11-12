9f12e6ade1984e7787acdd616c626f70e0c3678b
Emma Nam <en2458@gmail.com>
2020-11-12 17:31:30 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,16 +1,15 @@
-function forLoop(array){
-  for (let i = 0; i < 25; i++){
-    if(i===1){
-      array.push(`I am 1 strange loop.`);
-    }
-    else{
-      array.push(`I am ${i} strange loops.`);
-    }
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+    array.push("I am 1 strange loop.");
+  } else {
+    array.push(`I am ${i} strange loops.`);
   }
-    return array;
+}
+return array
 }
 
-function whileLoop(n){
+function whileLoop(n) {
   let countdown = n;
   while (countdown > 0) {
     console.log(--countdown);
@@ -18,12 +17,13 @@ function whileLoop(n){
   return 'done';
 }
 
-function doWhileLoop(num){
+function doWhileLoop(num) {
   let i = 0;
   function incrementVariable() {
-     i = i + 1;
+    i = i + 1;
     return i;
   }
+
   do {
     console.log("I run once regardless.");
   } while (incrementVariable() < num);
